a2dd0095e121a0a0ef3384d9d0aa59bcea6dd93d
S4ndr1n3 <antonietti.sandrine@gmail.com>
2025-04-26 18:27:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for anchor links
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
